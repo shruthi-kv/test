5c9037fc47000ea6344c20de9e563528f6a4a421
Shruthi KV <Shruthi.K.V@ibm.com>
2022-12-20 21:46:39 +0530

    changes made to match the course

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,19 +1,17 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
 import Card from "../UI/Card";
-import NewExpense from "../NewExpense/NewExpense";
+// import NewExpense from "../NewExpense/NewExpense";
 function Expenses(props){
-    const expense = [
-        {id:"e1", title:"title A", amount:21, date:new Date(2022,12,23)},
-        {id:"e2", title:"title B", amount:22, date:new Date(2022,12,24)},
-        {id:"e3", title:"title C", amount:23, date:new Date(2022,12,25)},
-      ]
-    
+    // const expense = [
+    //     {id:"e1", title:"title A", amount:21, date:new Date(2022,12,23)},
+    //     {id:"e2", title:"title B", amount:22, date:new Date(2022,12,24)},
+    //     {id:"e3", title:"title C", amount:23, date:new Date(2022,12,25)},
+    //   ]
     return<Card className="expenses">
-        <NewExpense/>
-        <ExpenseItem title={expense[0].title} amount={expense[0].amount} date={expense[0].date}></ExpenseItem>
-        <ExpenseItem title={expense[1].title} amount={expense[1].amount} date={expense[1].date}></ExpenseItem>
-        <ExpenseItem title={expense[2].title} amount={expense[2].amount} date={expense[2].date}></ExpenseItem>
+        <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
+        <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
+        <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
     </Card>
 
     }
