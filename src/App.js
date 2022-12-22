170ea14f75fac777006aec9b34241b5b98cc81b4
Shruthi KV <Shruthi.K.V@ibm.com>
2022-12-22 19:04:27 +0530

    for loop and key

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense'
 
 
+const DUMMY_EXPENSES = [
+  {id:"e1", title:"title A", amount:21, date:new Date(2022,12,23)},
+  {id:"e2", title:"title B", amount:22, date:new Date(2022,12,24)},
+  {id:"e3", title:"title C", amount:23, date:new Date(2022,12,25)},
+]
+
 
 function App() {
 
-  const expense = [
-    {id:"e1", title:"title A", amount:21, date:new Date(2022,12,23)},
-    {id:"e2", title:"title B", amount:22, date:new Date(2022,12,24)},
-    {id:"e3", title:"title C", amount:23, date:new Date(2022,12,25)},
-  ]
+ const [expenses,setExpenses] =  useState(DUMMY_EXPENSES)
+    const addExpenseHandler = (expense) =>{
   
-const addExpenseHandler = expense =>{
-  console.log("in the App.js");
-  console.log(expense)
+     setExpenses((prevExpenses) => {
+     return [expense, ...prevExpenses];
+  });
 }
 
   return (
     <div className="App">
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expense}></Expenses>
+      <Expenses items={expenses}></Expenses>
     </div>
   );
 }
